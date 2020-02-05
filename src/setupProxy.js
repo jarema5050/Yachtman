c552ef17a5becf40adf5909c6923510974fbff87
Jędrzej Sokołowski <jarema.zhr@gmail.com>
2020-02-05 08:52:29 +0100

    websocket error solution - proxymiddleware-vv3

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,2 @@
 const proxy = require('http-proxy-middleware');
-module.exports = function(app) {
-  app.use(
-    '/api',
-    proxy(
-        '/auth/google', { target: 'http://localhost:3003/' })
-  );
-};
+module.exports = function(app) { app.use(proxy('/auth/google', { target: 'http://localhost:3000/' } )); }
